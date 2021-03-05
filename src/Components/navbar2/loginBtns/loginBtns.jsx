e387367c38bb7f70142663edcf2f1a0da2a0f263
MuhammadTalhaKhan22784 <talhatariq0608@gmail.com>
2021-03-05 21:04:17 +0500

    all issues fixed

--- src/Components/navbar2/loginBtns/loginBtns.jsx
+++ src/Components/navbar2/loginBtns/loginBtns.jsx
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./style.css";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
-const Btns = ({ className }) => {
+const Btns = ({ className, closeFunc }) => {
+  let [func, setFunc] = useState(false);
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight);
+    return () => window.removeEventListener("resize", updateWidthAndHeight);
+  }, []);
+
+  if (width < 950) {
+    func = true;
+    // close = false
+  }
+
   const history = useHistory();
   return (
     <div className={className}>
       <div className="btns_div">
-        <button className="acount_btn" onClick={() => {history.push("/signup");}}>
+        <button
+          className="acount_btn"
+          onClick={() => {
+            history.push("/signup");
+            {
+              if (func) closeFunc();
+            }
+          }}
+        >
           Sign up
         </button>
-        <button className="acount_btn" onClick={() => {history.push("/signin");}}>
+        <button
+          className="acount_btn"
+          onClick={() => {
+            history.push("/signin");
+            {
+              if (func) closeFunc();
+            }
+          }}
+        >
           login
         </button>
         <span>
